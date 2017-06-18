d0829e866375b7e05e8b9b86bbf1e78bd3e196a2
Ildiko Czeller <czeildi@gmail.com>
2017-06-18 15:14:08 +0200

    fix factor levels of education

--- Meetup_2017_06_21/tidy_birth_data.R
+++ Meetup_2017_06_21/tidy_birth_data.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(magrittr)
+library(forcats)
 
 unzip("Meetup_2017_06_21/births_by_mother_age_and_education_raw.csv.zip",
       exdir = "Meetup_2017_06_21/")
@@ -21,6 +22,7 @@ cleaned_dt <- filtered %>%
     .[, age := as.integer(stringr::str_replace_all(age, ' years', ''))] %>% 
     .[, num_birth := as.integer(stringr::str_replace_all(num_birth, ' ', ''))] %>% 
     .[, education_level := as.factor(education_level)] %>% 
+    .[, education_level := fct_relevel(education_level, "Tertiary education (levels 5-8)", after = 2)] %>% 
     .[, country := as.factor(country)] %>% 
     as.data.frame()
 
