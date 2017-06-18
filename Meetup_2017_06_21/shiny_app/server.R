09cd899bcbcfa78cc4cffa7568a8844a80de607c
Ildiko Czeller <czeildi@gmail.com>
2017-06-18 14:11:34 +0200

    refactor common data source

--- Meetup_2017_06_21/shiny_app/server.R
+++ Meetup_2017_06_21/shiny_app/server.R
@@ -3,12 +3,14 @@ library("ggplot2")
 
 function(input, output) {
     
+    birth_dt <- readRDS('cleaned_birth_data.rds')
+    
     output$birth_dt <- renderDataTable(
-        readRDS('cleaned_birth_data.rds')
+        birth_dt
     )
     
     output$birth_plot <- renderPlot(
-        ggplot(readRDS('cleaned_birth_data.rds'), aes(x = age, y = num_birth, fill = education_level)) + 
+        ggplot(birth_dt, aes(x = age, y = num_birth, fill = education_level)) + 
             geom_col(position = 'dodge') + 
             facet_grid(year ~ country) + 
             theme(legend.position = 'bottom', legend.direction = 'vertical')
