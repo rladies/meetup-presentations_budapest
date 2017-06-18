1adc0b758d1556e0ef1f5ae27277a8633f44d88d
Ildiko Czeller <czeildi@gmail.com>
2017-06-18 14:39:22 +0200

    refactor dt filtering into simple function

--- Meetup_2017_06_21/shiny_app/server.R
+++ Meetup_2017_06_21/shiny_app/server.R
@@ -4,16 +4,20 @@ library("dplyr")
 
 function(input, output) {
     
-    birth_dt <- readRDS('cleaned_birth_data.rds')
+    filtered_birth_dt <- function() {
+        message('filtered birth dt function has been called')
+        filter(
+            readRDS('cleaned_birth_data.rds'),
+            year >= input$period[1] & year <= input$period[2]
+        )
+    }
     
     output$birth_dt <- renderDataTable(
-        birth_dt
+        filtered_birth_dt()
     )
     
     output$birth_plot <- renderPlot({
-        filtered_birth_dt <- filter(birth_dt, year >= input$period[1] & year <= input$period[2])
-        
-        ggplot(filtered_birth_dt, aes(x = age, y = num_birth, fill = education_level)) + 
+        ggplot(filtered_birth_dt(), aes(x = age, y = num_birth, fill = education_level)) + 
             geom_col(position = 'dodge') + 
             facet_grid(year ~ country) + 
             theme(legend.position = 'bottom', legend.direction = 'vertical')
