e2e017d3407a22a4e9f9417fc93e3de2244cd2d6
Ildiko Czeller <czeildi@gmail.com>
2017-06-18 13:50:20 +0200

    show computed result in ui

--- Meetup_2017_06_21/shiny_app/server.R
+++ Meetup_2017_06_21/shiny_app/server.R
@@ -1,5 +1,7 @@
 library("shiny")
 
 function(input, output) {
-    
+    output$birth_dt <- renderDataTable({
+        readRDS('cleaned_birth_data.rds')
+    })
 }
--- Meetup_2017_06_21/shiny_app/ui.R
+++ Meetup_2017_06_21/shiny_app/ui.R
@@ -1,3 +1,9 @@
 fluidPage(
-    title = "shiny-demo"
+    title = "births shiny-demo",
+    sidebarPanel(
+        p('for future widgets')
+    ),
+    mainPanel(
+        dataTableOutput("birth_dt")
+    )
 )
