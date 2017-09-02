1688109d580aaece83a4ec139f88119ee1dc5464
Ildiko Czeller <czeildi@gmail.com>
2017-09-02 19:55:01 +0200

    restore where we left off in June

--- Meetup_2017_09_21/eu_births_shiny_app/server.R
+++ Meetup_2017_09_21/eu_births_shiny_app/server.R
@@ -4,19 +4,21 @@ library("dplyr")
 
 function(input, output) {
     
-    getFilteredBirthDt <- function() {
-        message("filtered birth dt function has been called with ", input$period)
-        
-        readRDS("cleaned_birth_data.rds") %>%
-            filter(year >= input$period[1] & year <= input$period[2])
-    }
+    # getFilteredBirthDt <- function() {
+    #     message("filtered birth dt function has been called with ", input$period)
+    # 
+    #     readRDS("cleaned_birth_data.rds") %>%
+    #         filter(year >= input$period[1] & year <= input$period[2])
+    # }
     
     output$birth_dt <- renderDataTable(
-        getFilteredBirthDt()
+        readRDS("cleaned_birth_data.rds") %>%
+            filter(year >= input$period[1] & year <= input$period[2])
     )
     
     output$birth_summary_plot <- renderPlot({
-        getFilteredBirthDt() %>% 
+        readRDS("cleaned_birth_data.rds") %>%
+            filter(year >= input$period[1] & year <= input$period[2]) %>% 
             ggplot(aes(x = age, y = num_birth, fill = education_level)) + 
             geom_col(position = "dodge") + 
             facet_grid(year ~ country) + 
