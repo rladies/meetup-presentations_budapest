285d0b453ea5394458238349ad19974b222a388f
Ágnes Salánki <asalanki@hotels.com>
2018-03-09 00:43:53 +0000

    Final modifications

--- Meetup_2018_03_09/Data structures and loops/Data structures and loops/R/cheatsheet.R
+++ Meetup_2018_03_09/Data structures and loops/Data structures and loops/R/cheatsheet.R
@@ -6,8 +6,20 @@ y <- c("Jancsi", "Juliska")
 z <- c(TRUE, FALSE) 
 
 ## Nezzuk meg, az egyes indexek ([1], [2], [3] stb.) mivel ternek vissza.
-
-
+x[1]
+x[2]
+x[3]
+y[1]
+y[2]
+y[3]
+y[4]
+z[1]
+z[2]
+z[3]
+# Konlk 1: NA-val ter vissza, nem pedig out of bounds errorral
+# Konkl 2: egytol indexelunk
+# Konkl 3: csak azonos tipusu elemek kerulnek bele, egyebkent kasztolodik
+c(1, 2, FALSE)
 ################################################################################
 ####################### 2 Listak ###############################################
 ################################################################################
@@ -18,58 +30,116 @@ z <- list("a" = c(1, 2, 3))
 
 ## Nezzuk meg, az egyes listaindexek ([[1]], [[2]], [[3]] stb.) mivel
 ## ternek vissza!
+x[[1]]
+x[[2]]
+x[[3]]
+x[[4]]
+y[[2]][[2]]
+z[[1]][2]
+z[["a"]]
 
 ## Most nezzuk meg, a szimpla indexek ([1], [2], [3] stb.) mivel ternek vissza!
+x[1]
+x[1:2]
+y[1]
+
+# Konkl 1: ide barmi johet
+# Konkl 2: a nevesites sokkal jobban kijon
+# Subscript out of bounds-zal ter vissza
 
 ################################################################################
 ####################### 3 Data frame-ek ########################################
 ################################################################################
-rm(y, z)
-x <- data.frame(a = c(10, 20, 30),
+
+x <- data.frame(a = c(1, 2, 3),
                 b = c(TRUE, FALSE, FALSE))
 
 ## Milyen tipusu indexelesek mukodnek jol data frame-eknel? Nezzuk meg, hogy a
 ## [,],  [[]], $ operatorok mivel ternek vissza!
- 
+x[1, 2]
+x[2, 1]
+x[3, 13]
+x$a
+x[["a"]]
+x[[1]]
+x[1, 2]
+x[[2]][1]
 ################################################################################
 ####################### 4 A fantasztikus for ciklus ###########################
 ################################################################################
 
 ##  Irassuk ki az x data frame elso oszlopanak elemeit egyesevel!
-
+for(i in c(1, 2, 3)){
+  print(x[[1]][i])
+}
 
 ##  Szamoljuk az x data frame elso oszlopanak negyzetosszeget azokon a helyeken,
 ##  ahol a masodik oszlop erteke FALSE!
-
+negyzetosszeg <- 0
+for(i in c(1:3)){
+  if(x[[2]][i] == FALSE) {
+    negyzetosszeg <- negyzetosszeg + x[[1]][i] 
+  }
+}
+(negyzetosszeg)
 ################################################################################
-####################### 4 S/Lapply fuggveny ######################################
+####################### 4 Lapply fuggveny ######################################
 ################################################################################
 
 ##  Irassuk ki az x data frame elso oszlopanak elemeit egyesevel!
+sapply(X = c(1, 2, 3), FUN = function(index){
+  print(index)
+  T
+})
 
-
+sapply(X = x$a, FUN = function(item){
+  print(item)
+  T
+})
+# konkl: melyik a kenyelmesebb?
 
 ##  Szamoljuk az x data frame elso oszlopanak negyzetosszeget azokon a helyeken,
 ##  ahol a masodik oszlop erteke FALSE! Nezzuk meg, mi a kulonbseg a klasszikus
 ##  <- es a <<- operator kozott!
 
+negyzetosszeg <- 0
+
+sapply(X = c(1, 2, 3), FUN = function(index){
+  browser()
+  if(x$b[index] == FALSE) {
+    negyzetosszeg <<- negyzetosszeg + index^2 
+  }
+  T
+})
 
 ## Szamoljuk ki az x data frame elso oszlopaban talalhato szamok negyzetet es
 ## taroljuk el egy uj vektorban
 
+negyzetek <- c()
+for(i in c(1:3)) {
+  negyzetek <- c(negyzetek, x$a[i]^2)  
+}
+
+negyzetek2 <- sapply(x$a, FUN = function(item){
+  item^2
+  })
 
 ################################################################################
 ####################### 5 Purrr ################################################
 ################################################################################
 library(purrr)
 
-
 ##  Irassuk ki az x data frame elso oszlopanak elemeit egyesevel!
-
+map(x$a, .f = function(item){print(item)})
 
 ##  Szamoljuk az x data frame elso oszlopanak negyzetosszeget azokon a helyeken,
 ##  ahol a masodik oszlop erteke FALSE! Nezzuk meg, mi a kulonbseg a klasszikus
 ##  <- es a <<- operator kozott!
+negyzetosszeg <- 0
+map_if(x$a, x$b == FALSE, function(item){
+  negyzetosszeg <<- negyzetosszeg + item^2
+})
+
 
 ################################################################################
 ####################### 6 vektorizalas, ahol csak lehet ########################
@@ -77,5 +147,4 @@ library(purrr)
 
 ##  Szamoljuk az x data frame elso oszlopanak negyzetosszeget azokon a helyeken,
 ##  ahol a masodik oszlop erteke FALSE! 
-
-negyzetosszeg <- sum(x$a * !x$b)
+sum(x$a[x$b == FALSE]^2)
--- Meetup_2018_03_09/Data structures and loops/Data structures and loops/R/cheatsheet2.R
+++ Meetup_2018_03_09/Data structures and loops/Data structures and loops/R/cheatsheet2.R
@@ -7,7 +7,7 @@ library('dplyr')
 ## 1. feladat: talaljuk ki, mit jatszanak mostanaban a Katon szinpadan! ####
 ################################################################################
 
-url <- "http://katonajozsefszinhaz.hu/eloadasok/repertoar"
+url <- "http://katonajozsefszinhaz.hu/eloadasok/bemutatok"
 webpage <- read_html(url)
 
 eloadasok_html <- html_nodes(webpage, '#contents-news a')
@@ -15,24 +15,49 @@ eloadasok_cim <- html_text(eloadasok_html)
 
 ################################################################################
 ## 2. Feladat: hamozzuk ki az eloadasok relativ URL-jeit! ####
-## Hasznaljunk mondjuk for ciklust, kozben nezzunk ra a gsub es a 
-## regexpr hasznalatara!
 ################################################################################
+
+eloadasok_relativ <- c()
+
+for(i in c(1:length(eloadasok_cim))){
+  url_char <- as.character(eloadasok_html[[i]])
+  url_clear <- gsub("<a href=\"", "", url_char)
+  
+  searched_substring <- "\">"
+  searched_substring_index <- regexpr(searched_substring, url_clear, fixed = T)
+  eloadasok_relativ <- c(eloadasok_relativ, substring(url_clear, first = 1, last = searched_substring_index[[1]] - 1))
+}
 
 ################################################################################
 ## 3. Feladat: keszitsunk abszolut linkeket a relativ utak
 ## "http://katonajozsefszinhaz.hu/" prefixelesevel!
-## Hasznaljunk mondjuk vektorizalast!
 ################################################################################
+
+eloadasok_abszolut <- paste0("http://katonajozsefszinhaz.hu/", eloadasok_relativ)
 
 ################################################################################
 ## 4. Feladat: Scrape-elljuk az osszes eloadas adatlapjat es gyujtsuk ki 
-## a szereploket! Menjunk vegig peldaul az eloadas linkeken ehhez!
+## a szereploket
 ################################################################################
+
+szineszek <- list()
+lapply(eloadasok_abszolut, function(link){
+  print(link)
+  webpage <- read_html(link)
+  
+  eloadas_html <- html_nodes(webpage, '.cell-left-wide-highlighted a')
+  szineszek[[link]] <<- html_text(eloadas_html)
+})
 
 ################################################################################
 ## 5. eladat: Menjunk vegig a listan es gyujtsuk ki, a kedvenc szineszeink
-## jatszanak-e egyutt.
-## Ehhez mondjuk a szineszek listajan menjunk vegig indexelessel!
-## (A Fekete Erno sztringet igy keressuk: "Fekete Ern\U0151")
+## jatszanak-e egyutt
 ################################################################################
+
+lapply(c(1:length(szineszek)), function(index){
+  print(index)
+  if(any(szineszek[[index]] == "Keresztes Tamás") &
+     any(szineszek[[index]] == "Fekete Ern\U0151")) {
+    print(names(szineszek)[index])
+  } else{T}
+})
