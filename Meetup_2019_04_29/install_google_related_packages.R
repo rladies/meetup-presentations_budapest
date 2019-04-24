188082ffb52f992e58e809a2bf2ce51baa986768
Berta Bojte <berta.bojte@gmail.com>
2019-04-24 22:04:27 +0200

    Update install_google_related_packages.R

---
+++ Meetup_2019_04_29/install_google_related_packages.R
@@ -0,0 +1,6 @@
+# Google Calendar csomag telepítése
+install.packages('devtools')
+devtools::install_github("benjcunningham/googlecalendar")
+
+# Google Táblázat-kezelő csomag telepítése
+install.packages('googlesheets')
